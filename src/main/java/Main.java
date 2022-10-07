86aaed09bf1b1b223b541c510f3121c7c206084f
Stanislav Rakitov <prosperousrf@gmail.com>
2022-10-07 15:13:28 +0300

    Server and Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /**
  * @author Stanislav Rakitov
  */
@@ -5,9 +7,24 @@ public class Main {
   private static final int SERVER_PORT = 9999;
   private static final int THREAD_POOL_SIZE = 64;
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Server server = new Server(SERVER_PORT, THREAD_POOL_SIZE);
+
+
+    // добавление handler'ов (обработчиков)
+    server.addHandler("GET", "/messages", (request, responseStream) -> {
+      try {
+        server.responseWithoutContent(responseStream, "404", "Not Found");
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
+
+    server.addHandler("POST", "/messages", (request, responseStream) -> server.responseWithoutContent(responseStream, "503", "Service Unavailable"));
+
+    server.addHandler("GET", "/", ((request, outputStream) -> server.defaultHandler(outputStream, "index.html")));
+
+    // Start
     server.start();
   }
-
 }
