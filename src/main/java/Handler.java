33a5470824247a653d1ccc2c184620281b990fb4
Stanislav Rakitov <prosperousrf@gmail.com>
2022-10-07 13:22:06 +0300

    add throws in Handler

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -1,9 +1,10 @@
 import java.io.BufferedOutputStream;
+import java.io.IOException;
 
 /**
  * @author Stanislav Rakitov
  */
 @FunctionalInterface
 public interface Handler {
-  void handle(Request request, BufferedOutputStream outputStream);
+  void handle(Request request, BufferedOutputStream outputStream) throws IOException;
 }
